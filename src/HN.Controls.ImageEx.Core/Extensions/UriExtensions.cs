0477ec315e92f20e273ad709f5d6dea2885dab1b
h82258652 <h82258652@hotmail.com>
2019-02-18 13:48:31 +0800

    添加文档注释

--- src/HN.Controls.ImageEx.Core/Extensions/UriExtensions.cs
+++ src/HN.Controls.ImageEx.Core/Extensions/UriExtensions.cs
@@ -2,8 +2,17 @@
 
 namespace HN.Extensions
 {
+    /// <summary>
+    /// <see cref="Uri" /> 扩展类。
+    /// </summary>
     public static class UriExtensions
     {
+        /// <summary>
+        /// 获取 <see cref="Uri" /> 实例是否是 Http 协议。
+        /// </summary>
+        /// <param name="uri"><see cref="Uri" /> 实例。</param>
+        /// <returns><see cref="Uri" /> 实例是否是 Http 协议。</returns>
+        /// <exception cref="ArgumentNullException">uri 为 <see langword="null"/>。</exception>
         public static bool IsHttp(this Uri uri)
         {
             if (uri == null)
--- src/HN.Controls.ImageEx.Core/Services/IDesignModeService.cs
+++ src/HN.Controls.ImageEx.Core/Services/IDesignModeService.cs
@@ -1,7 +1,13 @@
 ﻿namespace HN.Services
 {
+    /// <summary>
+    /// 设计模式服务。
+    /// </summary>
     public interface IDesignModeService
     {
+        /// <summary>
+        /// 获取当前是否在设计模式下。
+        /// </summary>
         bool IsInDesignMode { get; }
     }
 }
