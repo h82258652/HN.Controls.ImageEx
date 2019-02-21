b15e9de03a65996eaa347e93629e278d3d8c3682
h82258652 <h82258652@hotmail.com>
2019-02-21 18:34:10 +0800

    添加文档注释

--- src/HN.Controls.ImageEx.Uwp/Media/ImageSurfaceFailedStatusException.cs
+++ src/HN.Controls.ImageEx.Uwp/Media/ImageSurfaceFailedStatusException.cs
@@ -3,8 +3,17 @@ using Windows.UI.Xaml.Media;
 
 namespace HN.Media
 {
+    /// <inheritdoc />
+    /// <summary>
+    /// 表示图像加载失败的错误。
+    /// </summary>
     public class ImageSurfaceFailedStatusException : Exception
     {
+        /// <inheritdoc />
+        /// <summary>
+        /// 初始化 <see cref="ImageSurfaceFailedStatusException" /> 类的新实例。
+        /// </summary>
+        /// <param name="status">图像加载的状态。</param>
         public ImageSurfaceFailedStatusException(LoadedImageSourceLoadStatus status)
         {
             if (!Enum.IsDefined(typeof(LoadedImageSourceLoadStatus), status))
@@ -15,6 +24,12 @@ namespace HN.Media
             Status = status;
         }
 
+        /// <summary>
+        /// 获取图像加载的状态。
+        /// </summary>
+        /// <returns>
+        /// 图像加载的状态。
+        /// </returns>
         public LoadedImageSourceLoadStatus Status { get; }
     }
 }
--- src/HN.Controls.ImageEx.Uwp/Media/SvgImageFailedStatusException.cs
+++ src/HN.Controls.ImageEx.Uwp/Media/SvgImageFailedStatusException.cs
@@ -3,8 +3,17 @@ using Windows.UI.Xaml.Media.Imaging;
 
 namespace HN.Media
 {
+    /// <inheritdoc />
+    /// <summary>
+    /// 表示 svg 图像加载失败的错误。
+    /// </summary>
     public class SvgImageFailedStatusException : Exception
     {
+        /// <inheritdoc />
+        /// <summary>
+        /// 初始化 <see cref="SvgImageFailedStatusException" /> 类的新实例。
+        /// </summary>
+        /// <param name="status"> svg 图像加载的状态。</param>
         public SvgImageFailedStatusException(SvgImageSourceLoadStatus status)
         {
             if (!Enum.IsDefined(typeof(SvgImageSourceLoadStatus), status))
@@ -15,6 +24,12 @@ namespace HN.Media
             Status = status;
         }
 
+        /// <summary>
+        /// 获取 svg 图像加载的状态。
+        /// </summary>
+        /// <returns>
+        /// svg 图像加载的状态。
+        /// </returns>
         public SvgImageSourceLoadStatus Status { get; }
     }
 }
