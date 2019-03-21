c1afedeed05a4ae517832a5afbfdcde12b719f66
h82258652 <h82258652@hotmail.com>
2019-03-21 16:44:24 +0800

    publish v1.0.5

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -5,8 +5,8 @@
 //------------------------------------------------------------------------------
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.4")]
-[assembly: AssemblyFileVersion("1.0.4")]
-[assembly: AssemblyInformationalVersion("1.0.4")]
+[assembly: AssemblyVersion("1.0.5")]
+[assembly: AssemblyFileVersion("1.0.5")]
+[assembly: AssemblyInformationalVersion("1.0.5")]
 [assembly: AssemblyCopyright("Copyright © h82258652 2018 - 2019")]
 
