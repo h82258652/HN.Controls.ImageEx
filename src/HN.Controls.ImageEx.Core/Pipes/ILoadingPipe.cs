7d978fe3a8de3bce0e9a03c086d328c4d363d104
h82258652 <h82258652@hotmail.com>
2019-05-29 14:35:49 +0800

    修正文档注释

--- src/HN.Controls.ImageEx.Core/Pipes/ILoadingPipe.cs
+++ src/HN.Controls.ImageEx.Core/Pipes/ILoadingPipe.cs
@@ -21,7 +21,7 @@ namespace HN.Pipes
         /// 执行管道加载逻辑。
         /// </summary>
         /// <param name="context">加载上下文。</param>
-        /// <param name="next">下一个加载管理的调用委托。</param>
+        /// <param name="next">下一个加载管道的调用委托。</param>
         /// <param name="cancellationToken">要监视取消请求的标记。</param>
         /// <returns>表示异步加载操作的任务。</returns>
         Task InvokeAsync([NotNull]ILoadingContext<TResult> context, [NotNull]LoadingPipeDelegate<TResult> next, CancellationToken cancellationToken = default(CancellationToken));
