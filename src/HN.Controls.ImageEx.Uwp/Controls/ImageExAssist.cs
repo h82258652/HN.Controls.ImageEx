4f46d8de3a58998fde23307efb956f4d4fc4dfeb
h82258652 <h82258652@hotmail.com>
2019-06-13 14:54:50 +0800

    添加ImageExAssist及其Demo

---
+++ src/HN.Controls.ImageEx.Uwp/Controls/ImageExAssist.cs
@@ -0,0 +1,47 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace HN.Controls
+{
+    public class ImageExAssist : ContentControl
+    {
+        public static readonly DependencyProperty SourceProperty = DependencyProperty.Register(nameof(Source), typeof(ImageEx), typeof(ImageExAssist), new PropertyMetadata(default(ImageEx)));
+
+        public ImageExAssist()
+        {
+            DefaultStyleKey = typeof(ImageExAssist);
+
+            Loaded += ImageExAssist_Loaded;
+            Unloaded += ImageExAssist_Unloaded;
+        }
+
+        public ImageEx Source
+        {
+            get => (ImageEx)GetValue(SourceProperty);
+            private set => SetValue(SourceProperty, value);
+        }
+
+        private void ImageExAssist_Loaded(object sender, RoutedEventArgs e)
+        {
+            DependencyObject parent = this;
+            do
+            {
+                parent = VisualTreeHelper.GetParent(parent);
+                if (parent is ImageEx imageEx)
+                {
+                    Source = imageEx;
+                    return;
+                }
+            }
+            while (parent != null);
+
+            Source = null;
+        }
+
+        private void ImageExAssist_Unloaded(object sender, RoutedEventArgs e)
+        {
+            Source = null;
+        }
+    }
+}
---
+++ src/HN.Controls.ImageEx.Wpf/Controls/ImageExAssist.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace HN.Controls
+{
+    public class ImageExAssist : ContentControl
+    {
+        public static readonly DependencyProperty SourceProperty;
+        private static readonly DependencyPropertyKey SourcePropertyKey = DependencyProperty.RegisterReadOnly(nameof(Source), typeof(ImageEx), typeof(ImageExAssist), new PropertyMetadata(default(ImageEx)));
+
+        static ImageExAssist()
+        {
+            SourceProperty = SourcePropertyKey.DependencyProperty;
+
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ImageExAssist), new FrameworkPropertyMetadata(typeof(ImageExAssist)));
+        }
+
+        public ImageExAssist()
+        {
+            Loaded += ImageExAssist_Loaded;
+            Unloaded += ImageExAssist_Unloaded;
+        }
+
+        public ImageEx Source
+        {
+            get => (ImageEx)GetValue(SourceProperty);
+            private set => SetValue(SourcePropertyKey, value);
+        }
+
+        private void ImageExAssist_Loaded(object sender, RoutedEventArgs e)
+        {
+            DependencyObject parent = this;
+            do
+            {
+                parent = VisualTreeHelper.GetParent(parent);
+                if (parent is ImageEx imageEx)
+                {
+                    Source = imageEx;
+                    return;
+                }
+            }
+            while (parent != null);
+
+            Source = null;
+        }
+
+        private void ImageExAssist_Unloaded(object sender, RoutedEventArgs e)
+        {
+            Source = null;
+        }
+    }
+}
