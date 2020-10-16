ddbf85bd0fa51cfecf2682b1a4601350e1f23f36
h82258652 <842053625@qq.com>
2020-10-16 17:21:58 +0800

    move uwp demo memory usage display to global

--- demo/UwpDemo/Views/MemoryUsageView.xaml.cs
+++ demo/UwpDemo/Views/MemoryUsageView.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Linq;
-using Windows.System;
 using Windows.UI.Xaml;
 
 namespace UwpDemo.Views
@@ -9,7 +8,6 @@ namespace UwpDemo.Views
     public sealed partial class MemoryUsageView
     {
         private readonly Stopwatch _stopwatch;
-        private readonly DispatcherTimer _timer;
 
         public MemoryUsageView()
         {
@@ -19,32 +17,12 @@ namespace UwpDemo.Views
             _stopwatch.Start();
 
             GridView.ItemsSource = Enumerable.Range(0, 100).Select(temp => "https://www.baidu.com/img/bd_logo1.png?t=" + Guid.NewGuid()).ToList();
-
-            _timer = new DispatcherTimer
-            {
-                Interval = TimeSpan.FromSeconds(0.2)
-            };
-            _timer.Tick += Timer_Tick;
         }
 
         private void MemoryUsageView_Loaded(object sender, RoutedEventArgs e)
         {
             _stopwatch.Stop();
             LoadTimeTextBlock.Text = $"Load time: {_stopwatch.ElapsedMilliseconds}ms";
-
-            _timer.Start();
-        }
-
-        private void MemoryUsageView_Unloaded(object sender, RoutedEventArgs e)
-        {
-            _timer.Stop();
-        }
-
-        private void Timer_Tick(object sender, object e)
-        {
-            var appMemoryUsage = MemoryManager.AppMemoryUsage;
-            var memoryUsage = Microsoft.Toolkit.Converters.ToFileSizeString((long)appMemoryUsage);
-            MemoryUsageTextBlock.Text = memoryUsage;
         }
     }
 }
--- demo/UwpDemo/Views/RootView.xaml.cs
+++ demo/UwpDemo/Views/RootView.xaml.cs
@@ -1,13 +1,23 @@
-﻿using Windows.UI.Xaml;
+﻿using System;
+using Windows.System;
+using Windows.UI.Xaml;
 
 namespace UwpDemo.Views
 {
     public sealed partial class RootView
     {
+        private readonly DispatcherTimer _timer;
+
         public RootView()
         {
             InitializeComponent();
             RootFrame.Navigate(typeof(MainView));
+
+            _timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(0.2)
+            };
+            _timer.Tick += Timer_Tick;
         }
 
         private void GoBackButton_Click(object sender, RoutedEventArgs e)
@@ -16,6 +26,23 @@ namespace UwpDemo.Views
             {
                 RootFrame.GoBack();
             }
+        }
+
+        private void RootView_Loaded(object sender, RoutedEventArgs e)
+        {
+            _timer.Start();
+        }
+
+        private void RootView_Unloaded(object sender, RoutedEventArgs e)
+        {
+            _timer.Stop();
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            var appMemoryUsage = MemoryManager.AppMemoryUsage;
+            var memoryUsage = Microsoft.Toolkit.Converters.ToFileSizeString((long)appMemoryUsage);
+            MemoryUsageTextBlock.Text = memoryUsage;
         }
     }
 }
